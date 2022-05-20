4b1b1da187a940d3e84d732365a461da5c6c6e2c
marco chavez <chavezcm@sheridancollege.ca>
2022-05-20 14:11:12 -0400

    this is my first commit

---
+++ src/student_spr2022/Student_spr2022.java
@@ -0,0 +1,35 @@
+package student_spr2022;
+import java.util.*; 
+
+public class Student_spr2022 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a word :");
+        String myWord = scan.nextLine();
+        //System.out.print(word);
+        //char[] test = new char[5];
+        //test[0]='M';
+        //test[1]='a';
+        //test[2]='R';
+        //test[3]='c';
+        //test[4]='O';
+        
+        char[] myLetter = new char[myWord.length()];
+                
+        for (int i = 0; i < myLetter.length; i++) {
+            myLetter[i] = myWord.charAt(i);
+        }
+ 
+        System.out.println("Printing in reverse order");
+        
+        for (int i = myLetter.length-1; i >= 0; i--){
+        System.out.println(myLetter[i]);
+        }       
+        
+        //for (char c : ch) {
+          //  System.out.println(c);
+        
+    }
+
+}
